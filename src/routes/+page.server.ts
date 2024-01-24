86b84ef7fd96818cb5a97105403313e5bc34d88b
Jack Buehner <jackb@furman.edu>
2024-01-24 15:06:48 -0500

    first commit

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,34 @@
+import { goto } from '$app/navigation';
+import { error, redirect } from '@sveltejs/kit';
+import type { Actions } from './$types';
+
+export const actions = {
+	guess: async ({ request }) => {
+		const data = await request.formData();
+		const name = data.get('guess-name');
+
+		const ageResult = fetch(`https://api.agify.io/?name=${name}`, { method: 'GET' })
+			.then((res) => res.json())
+			.then(({ age }) => age as number);
+
+		const genderResult = fetch(`https://api.genderize.io/?name=${name}`, { method: 'GET' })
+			.then((res) => res.json())
+			.then(
+				({ gender, probability }) =>
+					({ gender, probability } as { gender: 'male' | 'female'; probability: number })
+			);
+
+		const nationalityResult = fetch(`https://api.nationalize.io/?name=${name}`, { method: 'GET' })
+			.then((res) => res.json())
+			.then(({ country }) => country as { country_id: string; probability: number }[]);
+
+		const result = {
+			age: await ageResult,
+			gender: (await genderResult).gender,
+			genderProbability: (await genderResult).probability,
+			potentialNationalities: await nationalityResult
+		};
+
+		return { success: true, result };
+	}
+} satisfies Actions;
---
+++ src/routes/+page.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+	export let form;
+
+	const regionNames = new Intl.DisplayNames(['en'], { type: 'region' });
+</script>
+
+<h1>Let a computer guess your age, gender, and nationality.</h1>
+<p>Input your name to start.</p>
+
+<form method="POST" action="?/guess">
+	<label>
+		Your name
+		<input type="text" name="guess-name" id="guess-name" />
+	</label>
+	<button>Guess</button>
+</form>
+
+<p class="data-sources">
+	Data is from <a href="https://genderize.io/">genderize.io</a>,
+	<a href="https://nationalize.io/">nationalize.io</a>, and
+	<a href="https://agify.io/">agify.io</a>.
+</p>
+
+{#if form?.success}
+	<div class="result">
+		<p>The computer thinks you are {form.result.age} years old.</p>
+		<p>
+			It is {(form.result.genderProbability * 100).toFixed(0)}% confident that your gender is {form
+				.result.gender}.
+		</p>
+		<p style="margin-bottom: 0;">
+			The computer has also guessed five possible nationalities for you based on your name:
+		</p>
+		<ul>
+			{#each form.result.potentialNationalities as { country_id, probability }}
+				<li>{regionNames.of(country_id)} ({(probability * 100).toFixed(1)}% probable)</li>
+			{/each}
+		</ul>
+	</div>
+{/if}
+
+<style>
+	.result {
+		border: 1px solid black;
+		margin: 20px 0;
+		padding: 10px 20px;
+	}
+
+	:global(body) {
+		max-width: 600px;
+		margin: 0 auto;
+	}
+
+	.data-sources {
+		font-size: 13px;
+		font-style: italic;
+		opacity: 0.7;
+	}
+</style>
